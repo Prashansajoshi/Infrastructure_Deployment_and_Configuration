d098f713523244e72612b54ed81d88912764f5c2
prashansa <joshiprashansa11@gmail.com>
2024-06-21 11:23:34 +0545

    cahnged the iam instance profile name

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -24,6 +24,6 @@ resource "aws_iam_role_policy_attachment" "role_policy_attachment_SSM" {
 }
 
 resource "aws_iam_instance_profile" "this" {
-  name = "prashansa_iam_aws_instance_profile"
+  name = "prashansa_iam_aws_instance"
   role = aws_iam_role.this.name
 }
